79e2233a15ac0863567565825bc8241f1620f154
chaucodes <justinchau31@gmail.com>
2023-03-24 23:11:10 -0700

    added can selection

--- script.js
+++ script.js
@@ -2,6 +2,45 @@ const juvee = document.getElementById('juvee');
 const obstacle = document.getElementById('obstacle');
 const startGameButton = document.getElementById('startGameButton');
 
+let gameRunning = false;
+
+const canImages = [
+  'img/blueRazz.png',
+  'img/kiwiStraw.png',
+  'img/tropicalCrush.png',
+  'img/watermelonLime.png',
+];
+// Function to change the selected can
+function selectCan(index) {
+  // const juvee = document.getElementById('juvee');
+  juvee.style.background = `url(${canImages[index]})`;
+  juvee.style.backgroundSize = '50px 50px'; // Ensure the background size remains the same
+}
+
+function displayCanImages() {
+  const canSelection = document.getElementById('canSelection');
+
+  canImages.forEach((imgSrc, index) => {
+    const can = document.createElement('img');
+    can.src = imgSrc;
+    can.style.width = '50px';
+    can.style.height = '50px';
+    can.style.margin = '10px';
+    can.style.cursor = 'pointer';
+    can.setAttribute('data-index', index);
+
+    can.addEventListener('click', function () {
+      selectCan(index);
+      console.log('clicked');
+    });
+
+    canSelection.appendChild(can);
+  });
+}
+
+// Call the function to display the can images
+displayCanImages();
+
 function jump() {
   if (juvee.classList != 'jump') {
     juvee.classList.add('jump');
@@ -14,6 +53,7 @@ function jump() {
 
 function startGame() {
   if (obstacle.classList != 'startedGame') {
+    gameRunning = true;
     obstacle.classList.add('startedGame');
 
     let isAlive = setInterval(function () {
@@ -38,7 +78,9 @@ function startGame() {
     }, 10);
 
     document.addEventListener('keydown', function (event) {
-      jump();
+      if (gameRunning) {
+        jump();
+      }
     });
   }
 }
